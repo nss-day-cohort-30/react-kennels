188db1141d9380499ffec5fb16ea5f9ccba4476c
Steve Brownlee <chortlehoort@gmail.com>
2019-03-01 15:42:25 -0600

    Cardified employees

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"
+import person from "./person.png"
+import "./EmployeeList.css"
+import "./Employee.css"
 
 
 class EmployeeList extends Component {
@@ -12,14 +15,17 @@ class EmployeeList extends Component {
             <section className="employees">
             {
                 this.props.employees.map(employee =>
-                    <div key={employee.id}>
-                        <Link className="nav-link" to={`/employees/${employee.id}`}>
-                            {employee.name}
-                            <button onClick={() => {
-                                    this.props.fireEmployee(employee.id)
-                                }}
-                            >Fire</button>
-                        </Link>
+                    <div key={employee.id} className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                <img src={person} className="icon--person" />
+                                <button onClick={() => { this.props.fireEmployee(employee.id) }}
+                                >Fire</button>
+                            </h5>
+                            <Link className="nav-link" to={`/employees/${employee.id}`}>
+                                {employee.name}
+                            </Link>
+                        </div>
                     </div>
                 )
             }
